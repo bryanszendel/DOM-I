9a3decd8b9ce489c1087a2b4050b8fa5435a3cee
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-09 17:23:49 -0600

    timer is counting correctly now but WIP

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,57 +1,70 @@
+// TIMER that simply relays the actual time of day
+
+// function startTime() {
+//     let today = new Date();
+//     let m = today.getMinutes();
+//     let s = today.getSeconds();
+//     let ms = today.getMilliseconds();
+//     m = checkTime(m);
+//     s = checkTime(s);
+//     ms = checkTime(ms);
+//     document.getElementById('secondTens').innerHTML = m;
+//     document.getElementById('secondOnes').innerHTML = s;
+//     document.getElementById('msHundreds').innerHTML = ms;
+//     var t = setTimeout(startTime, 10);
+//     // document.getElementById('msTens').innerHTML = ms;
+// }
+
+// function checkTime(i) {
+//     if (i < 10) {i = '0' + i};
+//     return i;
+// }
+
+// window.addEventListener("load", (event) => {
+//     startTime();
+// });
+
+
+// STRETCH GOAL TIMER
+// set a variable equal to the HTML element for seconds which will increment up +1 whenever clicked
+
+// SECONDS
+let s = document.querySelector('#secondOnes');
+// button = document.querySelector('.button'); 
+// USE IF CLICK BUTTON IS UNCOMMENTED BELOW
+
+s.textContent = 0;
+let timer = window.setInterval(function() {
+    let incrementer = 1;
+    s.textContent = (Number(s.textContent) + Number(incrementer)) % 10;
+}, 1000);
+
+// button.addEventListener('click', () => {
+//     window.clearInterval(timer)
+// })
+
+// MILLISECONDS
+const msHundreds = document.querySelector('#msHundreds');
+
+msHundreds.textContent = 0;
+window.setInterval(function() {
+    let incrementer = Number(1);
+    msHundreds.textContent = (Number(msHundreds.textContent) + Number(incrementer)) % 100;
+}, 10);
+
+
+// THIS ALLOWS THE CLICK BUTTON TO TRIGGER THE INCREMENTER
+// s.textContent = 0;
+// button.addEventListener('click', () => {
+//     let incrementer = 1;
+//     s.textContent = Number(s.textContent) + Number(incrementer);
+//     s.textContent = s.textContent;
+//     console.log('s', s)
+// })
+
+
+
 // const tens = document.getElementById('secondTens');
-
-// console.log(plusOne())
-// let ones = document.querySelector('#secondOnes');
-
-// let myVar = setInterval(myTimer, 1000);
-// function myTimer() {
-//     numbers1 = document.getElementById('secondOnes').innerHTML;
-//     document.getElementById('secondOnes').innerHTML = numbers + Number(1);
-// }
-
-function startTime() {
-    let today = new Date();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    let ms = today.getMilliseconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    ms = checkTime(ms);
-    document.getElementById('secondTens').innerHTML = m;
-    document.getElementById('secondOnes').innerHTML = s;
-    document.getElementById('msHundreds').innerHTML = ms;
-    var t = setTimeout(startTime, 10);
-    // document.getElementById('msTens').innerHTML = ms;
-}
-
-function checkTime(i) {
-    if (i < 10) {i = '0' + i};
-    return i;
-}
-
-window.addEventListener("load", (event) => {
-    startTime();
-});
-
-
-// let thatNumber = 0;
-// ones.innerHTML = thatNumber;
-
-// console.log(thatNumber)
-// plusOne = () => {
-//     thatNumber += 1;
-//     return thatNumber;
-//     console.log(thatNumber);
-// }
-
-// let onesIncrementer = window.setInterval(() => {
-//     window.plusOne();
-// }, 1000);
-// onesIncrementer;
-
-// const msHundreds = document.getElementById('msHundreds');
 
 
 // const msTens = document.getElementById('msTens');
-
-// tens() = 
