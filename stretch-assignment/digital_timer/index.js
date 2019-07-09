92c98247418e123a32243ced8ad88eac97557f2b
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-08 21:16:44 -0600

    the timer is counting numbers

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,15 +1,39 @@
 // const tens = document.getElementById('secondTens');
 
 // console.log(plusOne())
-let ones = document.querySelector('#secondOnes');
+// let ones = document.querySelector('#secondOnes');
 
-let myVar = setInterval(myTimer, 1000);
-let numbers;
-function myTimer() {
-    numbers = document.getElementById('secondOnes').innerHTML = Number(0);
-    document.getElementById('secondOnes').innerHTML = numbers + Number(1);
+// let myVar = setInterval(myTimer, 1000);
+// function myTimer() {
+//     numbers1 = document.getElementById('secondOnes').innerHTML;
+//     document.getElementById('secondOnes').innerHTML = numbers + Number(1);
+// }
+
+function startTime() {
+    let today = new Date();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let ms = today.getMilliseconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkTime(ms);
+    document.getElementById('secondTens').innerHTML = m;
+    document.getElementById('secondOnes').innerHTML = s;
+    document.getElementById('msHundreds').innerHTML = ms;
+    var t = setTimeout(startTime, 10);
+    // document.getElementById('msTens').innerHTML = ms;
 }
-myVar;
+
+function checkTime(i) {
+    if (i < 10) {i = '0' + i};
+    return i;
+}
+
+window.addEventListener("load", (event) => {
+    startTime();
+});
+
+
 // let thatNumber = 0;
 // ones.innerHTML = thatNumber;
 
