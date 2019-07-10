df1f7590506c62b3e10e8a1ac341af0af4ffeec7
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-10 10:01:11 -0600

    start stop and reset buttons work

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,70 +1,73 @@
-// TIMER that simply relays the actual time of day
-
-// function startTime() {
-//     let today = new Date();
-//     let m = today.getMinutes();
-//     let s = today.getSeconds();
-//     let ms = today.getMilliseconds();
-//     m = checkTime(m);
-//     s = checkTime(s);
-//     ms = checkTime(ms);
-//     document.getElementById('secondTens').innerHTML = m;
-//     document.getElementById('secondOnes').innerHTML = s;
-//     document.getElementById('msHundreds').innerHTML = ms;
-//     var t = setTimeout(startTime, 10);
-//     // document.getElementById('msTens').innerHTML = ms;
-// }
-
-// function checkTime(i) {
-//     if (i < 10) {i = '0' + i};
-//     return i;
-// }
-
-// window.addEventListener("load", (event) => {
-//     startTime();
-// });
-
 
 // STRETCH GOAL TIMER
 // set a variable equal to the HTML element for seconds which will increment up +1 whenever clicked
 
-// SECONDS
-let s = document.querySelector('#secondOnes');
-// button = document.querySelector('.button'); 
-// USE IF CLICK BUTTON IS UNCOMMENTED BELOW
-
-s.textContent = 0;
-let timer = window.setInterval(function() {
-    let incrementer = 1;
-    s.textContent = (Number(s.textContent) + Number(incrementer)) % 10;
-}, 1000);
-
-// button.addEventListener('click', () => {
-//     window.clearInterval(timer)
-// })
-
-// MILLISECONDS
+const t = document.querySelector('#secondTens');
+const s = document.querySelector('#secondOnes');
 const msHundreds = document.querySelector('#msHundreds');
+const msTens = document.querySelector('#msTens');
 
+// SECOND TENS
+t.textContent = 0;
+function timer1() {
+    t1 = window.setInterval(function() {
+        let incrementer = 1;
+        t.textContent = (Number(t.textContent) + Number(incrementer)) % 10;
+    }, 10000)
+}
+
+// SECOND ONES
+s.textContent = 0;
+function timer2() {
+    t2 = window.setInterval(function() {
+        let incrementer = 1;
+        s.textContent = (Number(s.textContent) + Number(incrementer)) % 10;
+    }, 1000)
+}
+
+// MILLISECONDS - HUNDREDS
 msHundreds.textContent = 0;
-window.setInterval(function() {
-    let incrementer = Number(1);
-    msHundreds.textContent = (Number(msHundreds.textContent) + Number(incrementer)) % 100;
-}, 10);
+function timer3() {
+    t3 = window.setInterval(function() {
+        let incrementer = Number(1);
+        msHundreds.textContent = (Number(msHundreds.textContent) + Number(incrementer)) % 10;
+    }, 100);
+}
 
+// MILLISECONDS - TENS
+msTens.textContent = 0;
+function timer4() {
+    t4 = window.setInterval(function() {
+        let incrementer = Number(1);
+        msTens.textContent = (Number(msTens.textContent) + Number(incrementer)) % 10;
+    }, 10)
+}
 
-// THIS ALLOWS THE CLICK BUTTON TO TRIGGER THE INCREMENTER
-// s.textContent = 0;
-// button.addEventListener('click', () => {
-//     let incrementer = 1;
-//     s.textContent = Number(s.textContent) + Number(incrementer);
-//     s.textContent = s.textContent;
-//     console.log('s', s)
-// })
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', () => {
+    timer1();
+    timer2();
+    timer3();
+    timer4();
+})
 
+function clearAll() {
+    window.clearInterval(t1);
+    window.clearInterval(t2);
+    window.clearInterval(t3);
+    window.clearInterval(t4);
+}
 
+const stopButton = document.querySelector('#stop');
+stopButton.addEventListener('click', () => {
+    clearAll();
+})
 
-// const tens = document.getElementById('secondTens');
-
-
-// const msTens = document.getElementById('msTens');
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    clearAll();
+    t.textContent = 0;
+    s.textContent = 0;
+    msTens.textContent = 0;
+    msHundreds.textContent = 0;
+})
