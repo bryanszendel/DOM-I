0c4a9533b760aa63c389b2bff78320267988ed99
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-11 09:59:17 -0600

    timer is counting at start button, stops at 10 and turns red

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,68 +6,41 @@ const t = document.querySelector('#secondTens');
 const s = document.querySelector('#secondOnes');
 const msHundreds = document.querySelector('#msHundreds');
 const msTens = document.querySelector('#msTens');
+const digitDiv = document.querySelectorAll('.digit')
 
-// SECOND TENS
-t.textContent = 0;
-function timer1() {
-    t1 = window.setInterval(function() {
-        let incrementer = 1;
-        t.textContent = (Number(t.textContent) + Number(incrementer)) % 10;
-    }, 10000)
-}
+function timer(node, time) {
+    let count = 0;
+    let digit = window.setInterval(function() {
+        if (count === 9) {
+            count = 0; 
+        } else count++;
+        node.textContent = count;
 
-// SECOND ONES
-s.textContent = 0;
-function timer2() {
-    t2 = window.setInterval(function() {
-        let incrementer = 1;
-        s.textContent = (Number(s.textContent) + Number(incrementer)) % 10;
-    }, 1000)
-}
+        if (t.textContent === '1') {
+            window.clearInterval(digit)
+            digitDiv.forEach((item) => item.classList.add('redDigit'))
+        }
+    }, time)
 
-// MILLISECONDS - HUNDREDS
-msHundreds.textContent = 0;
-function timer3() {
-    t3 = window.setInterval(function() {
-        let incrementer = Number(1);
-        msHundreds.textContent = (Number(msHundreds.textContent) + Number(incrementer)) % 10;
-    }, 100);
-}
-
-// MILLISECONDS - TENS
-msTens.textContent = 0;
-function timer4() {
-    t4 = window.setInterval(function() {
-        let incrementer = Number(1);
-        msTens.textContent = (Number(msTens.textContent) + Number(incrementer)) % 10;
-    }, 10)
+        const stopButton = document.querySelector('#stop');
+        stopButton.addEventListener('click', () => {
+        window.clearInterval(digit);
+    })
 }
 
 const startButton = document.querySelector('#start');
 startButton.addEventListener('click', () => {
-    timer1();
-    timer2();
-    timer3();
-    timer4();
-})
+    timer(t, 10000);
+    timer(s, 1000);
+    timer(msHundreds, 100);
+    timer(msTens, 10);
 
-function clearAll() {
-    window.clearInterval(t1);
-    window.clearInterval(t2);
-    window.clearInterval(t3);
-    window.clearInterval(t4);
-}
-
-const stopButton = document.querySelector('#stop');
-stopButton.addEventListener('click', () => {
-    clearAll();
 })
 
 const resetButton = document.querySelector('#reset');
 resetButton.addEventListener('click', () => {
-    clearAll();
-    t.textContent = 0;
-    s.textContent = 0;
-    msTens.textContent = 0;
-    msHundreds.textContent = 0;
+    t.textContent = '-';
+    s.textContent = '-';
+    msTens.textContent = '-';
+    msHundreds.textContent = '-';
 })
